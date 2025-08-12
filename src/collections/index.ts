d10eb10ac0fa96a970d5f6ea9592ecb651ea30db
Hien Nguyen <hiennc@biso24.com>
2025-08-12 10:27:10 +0700

    FEAT update style

--- src/collections/index.ts
+++ src/collections/index.ts
@@ -15,24 +15,30 @@ export const COLLECTIONS = {
   // Tenant Management Collections
   TENANT_COLLECTION_NAME: "tenant",
   TENANT_REPOSITORY_NAME: "tenants",
+
+  // Account Management Collections
   ACCOUNT_COLLECTION_NAME: "account",
   ACCOUNT_REPOSITORY_NAME: "accounts",
+
+  // Git project
   GIT_PROJECT_COLLECTION_NAME: "git_project",
   GIT_PROJECT_REPOSITORY_NAME: "git_projects",
+
+  // Role Management Collections
   ROLE_COLLECTION_NAME: "role",
   ROLE_REPOSITORY_NAME: "roles",
+
+  // Git branch
   SERVER_COLLECTION_NAME: "server",
   SERVER_REPOSITORY_NAME: "servers",
+
+  // Git commit
   SERVICE_COLLECTION_NAME: "service",
   SERVICE_REPOSITORY_NAME: "services",
+
+  // Git commit
   SETTING_COLLECTION_NAME: "setting",
   SETTING_REPOSITORY_NAME: "settings",
-  REFRESH_TOKEN_COLLECTION_NAME: "refresh_token",
-  REFRESH_TOKEN_REPOSITORY_NAME: "refresh_tokens",
-  TENANT_USER_REPOSITORY_NAME: "tentant_users",
-  TENANT_USER_COLLECTION_NAME: "tenant_user",
-  TENANT_ROLE_REPOSITORY_NAME: "tentant_roles",
-  TENANT_ROLE_COLLECTION_NAME: "tentant_role",
 
   // Common Management Collections
   COMMON_COLLECTION_NAME: "common",
